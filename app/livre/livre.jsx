6c4c9303b52b13fb36345b8a48446f3eb3e84b42
mando_christ <mandochrist13@gmail.com>
2025-04-14 12:26:02 +0100

    Projet flipbooks Next.js

---
+++ app/livre/livre.jsx
@@ -0,0 +1,13 @@
+const Livre1 = () => {
+    return (
+      <iframe
+        src="/livres/livre1/index.html"
+        width="100%"
+        height="1000px"
+        style={{ border: 'none' }}
+      />
+    );
+  };
+  
+  export default Livre1;
+  

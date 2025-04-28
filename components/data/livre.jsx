53ee29c2a7aa8d402eb54fb2e5559f6d3ce96760
mando_christ <mandochrist13@gmail.com>
2025-04-28 18:29:41 +0100

    ajout des livres

--- components/data/livre.jsx
+++ components/data/livre.jsx
@@ -16,6 +16,18 @@ const livres = [
       chemin: "/livres/newtext/index.html",
       motDePasse: "Mcm@Natur%?25",
       titre: "La santé pour tous au naturel",
+    },
+    {
+      id: 4,
+      chemin: "/livres/La-Sexualit-de-la-Femme-au-Gabon.html",
+      motDePasse: "Srn@Sexuali%25",
+      titre: "La Sexualit de la Femme au Gabon",
+    },
+    {
+      id: 5,
+      chemin: "/livres/La-N-cessit-d-tre-un-Bon-Commercial-en-Tant-que-Comptable-S-adapter-pd.pdf.html",
+      motDePasse: "Srn@Commerc%25",
+      titre: "La N'eccessité d'être un Bon Commercial en Tant que Comptable S'adapter",
     }
   ];
   

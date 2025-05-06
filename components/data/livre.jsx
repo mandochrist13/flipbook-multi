5e240b1f6cf6dedb2b0021530bcdffed3bcf945b
mando_christ <mandochrist13@gmail.com>
2025-05-06 13:59:20 +0100

    ajout du livre 6 enjeu de lentrepreneuriat au gabon

--- components/data/livre.jsx
+++ components/data/livre.jsx
@@ -28,6 +28,12 @@ const livres = [
       chemin: "/livres/La-N-cessit-d-tre-un-Bon-Commercial-en-Tant-que-Comptable-S-adapter-pd.pdf.html",
       motDePasse: "Srn@Commerc%25",
       titre: "La N'eccessité d'être un Bon Commercial en Tant que Comptable S'adapter",
+    },
+    {
+      id: 6,
+      chemin: "/livres/enjeu/index.html",
+      motDePasse: "Srn@Entreup%25",
+      titre: "Les enjeux de l’entreprenariat en Afrique : cas du Gabon",
     }
   ];
   

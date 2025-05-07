e1939976a827fe132dd8baf0435b6f77bc4621b7
mando_christ <mandochrist13@gmail.com>
2025-05-07 18:21:47 +0100

    ajout des livres

--- components/data/livre.jsx
+++ components/data/livre.jsx
@@ -1,31 +1,31 @@
 const livres = [
     {
       id: 1,
-      chemin: "/livres/sant-/index.html",
+      chemin: "/livres/Publications/sant-/index.html",
       motDePasse: "Mcm@Sante%?25",
       titre: "La santé par l'alimentation adaptée à ton groupe sanguin",
     },
     {
       id: 2,
-      chemin: "/livres/conseil/index.html",
+      chemin: "/livres/Publications/conseil/index.html",
       motDePasse: "Mcm@Conseil%25",
       titre: "Conseil d'un père",
     },
     {
       id: 3,
-      chemin: "/livres/newtext/index.html",
+      chemin: "/livres/Publications/La-sant-pour-tous-au-naturel-V2/index.html",
       motDePasse: "Mcm@Natur%?25",
       titre: "La santé pour tous au naturel",
     },
     {
       id: 4,
-      chemin: "/livres/La-Sexualit-de-la-Femme-au-Gabon.html",
+      chemin: "/livres/Publications/La-Sexualit-de-la-Femme-au-Gabon/index.html",
       motDePasse: "Srn@Sexuali%25",
       titre: "La Sexualit de la Femme au Gabon",
     },
     {
       id: 5,
-      chemin: "/livres/La-N-cessit-d-tre-un-Bon-Commercial-en-Tant-que-Comptable-S-adapter-pd.pdf.html",
+      chemin: "/livres/Publications/La-N-cessit-d-tre-un-Bon-Commercial-en-Tant-que-Comptable-S-adapter-pd.pdf/index.html",
       motDePasse: "Srn@Commerc%25",
       titre: "La N'eccessité d'être un Bon Commercial en Tant que Comptable S'adapter",
     },
